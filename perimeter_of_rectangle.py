97317cc9d773c70179d60491468bbba9ce8dfbe5
Shriram Hegde <hegdeshriram82@gmail.com>
2026-02-18 22:15:57 +0530

    A simple python program that illustrates perimeter of the rectangle

---
+++ perimeter_of_rectangle.py
@@ -0,0 +1,5 @@
+# a simple python script that calculates perimeter of the rectangle
+length=int(input("length"))
+breadth=int(input("breadth"))
+perimeter=(2*length+2*breadth)
+print(f"perimeter of the rectangle is {perimeter} cm")
