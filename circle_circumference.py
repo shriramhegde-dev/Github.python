d788a9c60e7eb363999156251bb4224ddc0ba9e8
Shriram Hegde <hegdeshriram82@gmail.com>
2026-02-19 23:16:52 +0530

    A simple python program that calculates circumference of the circle

---
+++ circle_circumference.py
@@ -0,0 +1,5 @@
+# A python program that calculates circumference of the circle
+radius=int(input("radius"))
+PI=3.14
+Circumference=2*PI*radius
+print(f"circumference of the circle is {Circumference}")
