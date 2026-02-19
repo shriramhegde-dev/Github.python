66fa6e8a98611b1bf6f6b1187025849130375401
Shriram Hegde <hegdeshriram82@gmail.com>
2026-02-19 23:03:44 +0530

    A simple python script that calculates total marks

---
+++ total_marks.py
@@ -0,0 +1,12 @@
+# A python program that calculates marks of all of the subjects 
+print("please enter your subjects")
+subj1=int(input("subj1"))
+subj2=int(input("subj2"))
+subj3=int(input("subj3"))
+subj4=int(input("subj4"))
+subj5=int(input("subj5"))
+subj6=int(input("subj6"))
+subj7=int(input("subj7"))
+subj8=int(input("subj8"))
+total_marks=subj1+subj2+subj3+subj4+subj5+subj6+subj7+subj8
+print(f" your total marks is {total_marks}")
