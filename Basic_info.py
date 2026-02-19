ac11984ef0f0b66d0e72b56343323ee8ad74fe42
Shriram Hegde <hegdeshriram82@gmail.com>
2026-02-19 23:11:09 +0530

    A simple python script that takes name,class,DOB from user and displays it.

---
+++ Basic_info.py
@@ -0,0 +1,8 @@
+# A simple python program that takes your name,class,DOB 
+print("please enter your name")
+name=input("name")
+print("please enter your class")
+user_class=(int(input("user_class")))
+print("please enter your DOB")
+DOB=input("DOB")
+print(f" your name is {name} \n class is {user_class} \n DOB is {DOB}")
