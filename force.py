4fea4acad6f42956bec719a4509fca71aa899b09
Shriram Hegde <hegdeshriram82@gmail.com>
2026-02-20 23:41:01 +0530

    A simple python script that calculates force

---
+++ force.py
@@ -0,0 +1,7 @@
+# a simple python script that calculates the force
+print("please enter the mass")
+mass=float(input("mass"))
+print("please enter the accleration")
+accleration=float(input("accleration"))
+force=mass*accleration
+print(f"force is {force} newtons")
