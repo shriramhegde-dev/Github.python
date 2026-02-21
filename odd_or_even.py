867da8bb24600fc8cd1ea21597829fa1474372ff
Shriram Hegde <hegdeshriram82@gmail.com>
2026-02-21 23:01:48 +0530

    A simple python script that calculates whether a given number is odd or even

---
+++ odd_or_even.py
@@ -0,0 +1,7 @@
+# A simple python program that illustrates whether a given number is odd or even 
+print("please enter a number")
+number=int(input("number"))
+if number%2==0:
+    print("number is even")
+else:
+    print("given number is odd")
