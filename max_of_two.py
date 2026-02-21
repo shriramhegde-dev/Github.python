195a5830de49990a78e71f4cd8c1759ae3909f0e
Shriram Hegde <hegdeshriram82@gmail.com>
2026-02-21 23:12:30 +0530

    A simple python script that calculates maximum of two numbers

---
+++ max_of_two.py
@@ -0,0 +1,7 @@
+print("please enter two numbers")
+num1=int(input("enter the first number here"))
+num2=int(input("enter the second number here"))
+if num1>num2:
+    print(f"number 1:- {num1} is greater than number 2:-{num2} ")
+else:
+    print(f"number:2- {num2} is greater than number 1:-{num1} ")
